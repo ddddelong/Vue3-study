aa2155e31f9222f7c0dcf10ce6c186fe58f3faab
Amon <2219924419@qq.com>
2024-11-01 18:59:51 +0800

    引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import {createPinia} from 'pinia'
 // import router from './router'
 
+// 创建pinia实例
+const pinia = createPinia()
 const app = createApp(App)
 
 // app.use(router)
-
+app.use(pinia)    // 安装pinia插件
 app.mount('#app')
