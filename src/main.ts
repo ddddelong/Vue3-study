667be04387e8e08a99649d2d46d770c33383a8c8
Amon <2219924419@qq.com>
2024-11-13 21:01:58 +0800

    安装使用了路由和element plus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import {createPinia} from 'pinia'
-// import router from './router'
+import router from './router'
+import ElementPlus from 'element-plus';  // 引入element-plus组件库
+import 'element-plus/dist/index.css';  // 引入element-plus组件库样式
 
 // 创建pinia实例
 const pinia = createPinia()
 const app = createApp(App)
 
-// app.use(router)
+app.use(ElementPlus)  // 使用element-plus组件库
+app.use(router)
 app.use(pinia)    // 安装pinia插件
 app.mount('#app')
